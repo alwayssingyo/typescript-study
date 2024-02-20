37b5bb67afb3317a3bd11ce169334614be3eecc7
alwayssingyo <alwayssingyo@gmail.com>
2024-02-20 17:52:23 +0900

    feat : Getter, Setter 활용

--- 3-oop/3-3-encapsulation.ts
+++ 3-oop/3-3-encapsulation.ts
@@ -49,5 +49,25 @@
   const maker = CoffeMaker.makeMachine(32);
   maker.fillCoffeBeans(32);
 
-  console.log(maker);
+  class User {
+    // getter
+    get fullName(): string {
+      return `${this.firstName} ${this.lastName}`;
+    }
+    private internalAge = 4;
+    get age(): number {
+      return this.internalAge;
+    }
+    set age(num: number) {
+      if (num < 0) {
+        throw new Error('not avaiable age');
+      }
+      this.internalAge = num;
+    }
+    constructor(private firstName: string, private lastName: string) {}
+  }
+
+  const user = new User('Steve', 'Jobs');
+  user.age = 6;
+  console.log(user.age);
 }
