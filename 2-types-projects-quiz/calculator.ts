f84fb2246566e4cb0ca0370767462cd5c8f52b4e
alwayssingyo <alwayssingyo@gmail.com>
2024-02-20 12:15:37 +0900

    feat : 계산기 함수 추가

--- 2-types-projects-quiz/calculator.ts
+++ 2-types-projects-quiz/calculator.ts
@@ -6,3 +6,21 @@ console.log(calculate('substract', 3, 1)); // 2
 console.log(calculate('multiply', 4, 2)); // 8
 console.log(calculate('divide', 4, 2)); // 2
 console.log(calculate('remainder', 5, 2)); // 1
+
+type ActionType = 'add' | 'substract' | 'multiply' | 'divide' | 'remainder';
+function calculate(action: ActionType, a: number, b: number): number {
+  switch (action) {
+    case 'add':
+      return a + b;
+    case 'substract':
+      return a - b;
+    case 'multiply':
+      return a * b;
+    case 'divide':
+      return a / b;
+    case 'remainder':
+      return 5 % 2;
+    default:
+      throw new Error('unknown error');
+  }
+}
